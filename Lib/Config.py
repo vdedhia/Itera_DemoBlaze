943f3d43b84106933cd217ffae7e54d8f535090e
FHVaibhav <85790333+vdedhia@users.noreply.github.com>
2022-05-18 15:30:06 +0200

    Initial commit

---
+++ Lib/Config.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Config(Enum):
+    Base_URL = "https://opensource-demo.orangehrmlive.com/"
+    Test_Data = './Lib/Test_Data.txt'
+    Wait_Time_Sec = 5
+    Users = ['Raj', 'Kanagamanivelan', 'Paul']
